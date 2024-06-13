adf6db719cf32580695e313f22e4d19c621ab51a
valdimir-makarov <2010049@student.ruet.ac.bd>
2024-06-14 04:42:23 +0600

    this is the starting point

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 2000, 100);
+};
